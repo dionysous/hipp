d0ae6b564349c0e76f5f174ff22684aad490eb02
Dennis Hipp <dennis.hipp@regiohelden.de>
2018-05-31 17:16:14 +0200

    :truck: Move sass and js files to assets folder

--- functions.php
+++ functions.php
@@ -122,9 +122,9 @@ add_action( 'widgets_init', 'hipp_widgets_init' );
 function hipp_scripts() {
 	wp_enqueue_style( 'hipp-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'hipp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'hipp-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'hipp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'hipp-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
